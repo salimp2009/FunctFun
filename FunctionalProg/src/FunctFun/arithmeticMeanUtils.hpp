fb4b8f6fd2f02fd533c8e07f18cd723e33022183
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-02-09 17:29:58 +0300

    tested some duration versions somehow got too complicated need to find an easier way

--- FunctionalProg/src/FunctFun/arithmeticMeanUtils.hpp
+++ FunctionalProg/src/FunctFun/arithmeticMeanUtils.hpp
@@ -13,23 +13,53 @@
 
 namespace functfun
 {
-    // FIXME: add some doxygen & test with chrono durations :)
+    // FIXME: Testing duration version but did not work
+    template<typename T>
+    concept DurationDouble  = std::chrono::treat_as_floating_point_v<typename T::rep>;
+
+    template<typename T>
+    concept DurationInt     = not DurationDouble<T>;
+
+    template<typename T>
+    concept Duration        = DurationDouble<T> || DurationInt<T>;
+
+
     constexpr auto mean = []<typename R, typename init=double>(const R&& range)
     {
-               return std::transform_reduce(std::ranges::begin(range), std::ranges::end(range),
-                                     init{},
+               return std::transform_reduce(std::ranges::cbegin(range), std::ranges::cend(range),
+                                     init{0.0},
                                      std::plus<>(),
-                                     [&](auto elem) { return elem/static_cast<double>(std::size(range));});
+                                     [&](const auto& elem) { return elem/static_cast<double>(std::size(range));});
     };
+
+    // FIXME: Testing duration version but did not work
+    constexpr auto meanDouble = []<typename R, typename init=std::chrono::duration<long double>>(const R&& range)
+    {
+        return std::transform_reduce(std::ranges::cbegin(range), std::ranges::cend(range),
+                                     init{0.0},
+                                     std::plus<>(),
+                                     [&](const auto& elem) { return elem/static_cast<double>(std::size(range));});
+    };
+
 
     template<std::ranges::viewable_range R>
     requires std::ranges::forward_range<R>
     constexpr auto slidingMean(R&& range, std::size_t sampleCount)
     {
         return range | ranges::views::sliding(sampleCount)
-                     | ranges::views::transform(mean)
-                     | ranges::to<std::vector>();
+               | ranges::views::transform(mean)
+               | ranges::to<std::vector>();
     }
+
+    // FIXME: Testing duration version but did not work
+//    template<std::ranges::viewable_range R>
+//    requires std::ranges::forward_range<R> && Duration<std::ranges::range_value_t<R>>
+//    constexpr auto slidingMean(R&& range, std::size_t sampleCount)
+//    {
+//        return range | ranges::views::sliding(sampleCount)
+//               | ranges::views::transform(meanDouble)
+//               | ranges::to<std::vector>();
+//    }
 
 
 } // end of namespace
