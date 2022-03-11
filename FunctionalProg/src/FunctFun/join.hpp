d63e58e101be0bd6f9e19dc57c3a1f5237a768bb
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-03-11 16:11:28 +0300

    modified the 2nd version of join that does not take destination and returns a string but not happy with it since it is not constexpr and too expensive. May convert it to ranges version and returns a range without using a string inside the function

--- FunctionalProg/src/FunctFun/join.hpp
+++ FunctionalProg/src/FunctFun/join.hpp
@@ -14,13 +14,9 @@ namespace functfun
     //  and also change name join_toString
     //  try to add some more constraint if makes sense
 
-    // FIXME: added Projection but need to revise std::invocable<> concept to match it
-    //  needs to be tested
-
     template<std::input_iterator It, typename OutputIt, typename T, class Proj= std::identity,
              std::indirectly_unary_invocable<std::projected<It, Proj>> Pred>
-
-    auto join(It first, It last, OutputIt dest, T&& delimiter, Pred pred, Proj proj= {}) ->OutputIt
+    constexpr auto join(It first, It last, OutputIt dest, T&& delimiter, Pred pred, Proj proj= {}) ->OutputIt
     {
         if(first == last) return std::forward<OutputIt>(dest);
 
@@ -36,18 +32,23 @@ namespace functfun
     }
 
 
-    template<std::input_iterator It,  typename T,  std::invocable<std::iter_reference_t<It>> Pred>
-    auto join(It first, It last,  T&& delimiter, Pred pred) ->std::string
+    // FIXME: this is not quite; no constexpr because of std::string
+    //  and not sure the move inside the insert have any effect
+    // FIXME: might delete this option; it is too expensive; requires effort
+    //  to get compile time std::string
+    template<std::input_iterator It,  typename T,  class Proj= std::identity,
+             std::indirectly_unary_invocable<std::projected<It, Proj>> Pred>
+    auto join(It first, It last,  T&& delimiter, Pred pred, Proj proj={})->std::string
     {
         if(first == last) return {};
 
-        auto newStr = std::invoke(std::forward<Pred>(pred), *first);
+        auto newStr = std::invoke(std::forward<Pred>(pred), std::invoke(std::forward<Proj>(proj), *first));
         std::string result{ std::move(newStr)};
 
         for(++first; first !=last; ++first)
         {
-            newStr = delimiter + std::invoke(std::forward<Pred>(pred), *first);
-            result.insert(result.end(), std::move(newStr));
+            newStr = delimiter + std::invoke(std::forward<Pred>(pred), std::invoke(std::forward<Proj>(proj), *first));
+            result.insert(result.end(), std::move(std::begin(newStr)), std::move(std::end(newStr)));
         }
 
         return result;
