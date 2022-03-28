6efdd858af2c50d948111fb868e2775defef040b
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-03-28 16:44:07 +0300

    completed one overload for optionalWrap that inherit from std::optional started the next overload that does not require std::optional if it is a semiregular type; directly store with [[no_unique_address]] to optimize need to write of the interface so that it will work like the one with std::optional

--- FunctionalProg/src/FunctFun/rangeutils.hpp
+++ FunctionalProg/src/FunctFun/rangeutils.hpp
@@ -10,9 +10,15 @@ namespace functfun
     namespace details
     {
         template<typename T>
-        concept boxable = std::copy_constructible<T> && std::is_object_v<T>;
+        concept wrapable = std::copy_constructible<T> && std::is_object_v<T>;
 
-        template<boxable T>
+        template<typename T>
+        concept notCopyable = not std::copyable<T>;
+
+        template<typename T>
+        concept notMovable = not std::movable<T>;
+
+        template<wrapable T>
         struct optionalWrap : std::optional<T>
         {
             using std::optional<T>::optional;
@@ -20,10 +26,49 @@ namespace functfun
             constexpr optionalWrap() noexcept(std::is_nothrow_default_constructible_v<T>)
                     requires std::default_initializable<T>
                : std::optional<T>{std::in_place} { }
-            constexpr optionalWrap(const optionalWrap& ) =default;
-            constexpr optionalWrap(optionalWrap&& ) =default;
+            constexpr optionalWrap(const optionalWrap& ) = default;
+
+            // FIXME: clang tidy warn to make noexcept but the we need
+            //  to specify the conditional noexcept which maynot be defaulted then??
+            constexpr optionalWrap(optionalWrap&& ) = default;
+
+            using std::optional<T>::operator=;
+
+            constexpr optionalWrap& operator=(const optionalWrap& other)
+            noexcept(std::is_nothrow_copy_constructible_v<T>) requires notCopyable<T>
+            {
+                if(static_cast<bool>(other))
+                { this->emplace(*other); }
+                else
+                { this->reset();}
+                return *this;
+            }
+
+            constexpr optionalWrap& operator=(optionalWrap&& other)
+                    noexcept(std::is_nothrow_move_constructible_v<T>) requires notMovable<T>
+            {
+                if(static_cast<bool>(other))
+                { this->emplace(std::move(*other)); }
+                else
+                { this->reset();}
+                return *this;
+            }
+        };
+
+        // For type that are semiregular no need for optional
+        template<wrapable T>
+        requires std::semiregular<T>
+        struct optionalWrap<T>
+        {
+        private:
+            [[no_unique_address]] T mValue = T();
+
+        public:
+            constexpr optionalWrap()=default;
 
         };
+
+
 
     } //endof namespace details
 
