399924ec37908878c5a1579276bd766417a35370
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-03-31 23:57:09 +0300

    complete a very naive implementation of expression template to do arithmetic addition and multiplication of 2 vectors which creates a lazy evaluation of doing elementwise add and multiplication that will be called upon calling; will test then start a new range adaptor

--- FunctionalProg/src/FunctFun/arithmeticVector.hpp
+++ FunctionalProg/src/FunctFun/arithmeticVector.hpp
@@ -10,6 +10,117 @@
 namespace functfun
 {
 
+    // A naive example for a expression template from Rainer Grimm's example
+    template<typename T, typename Cont= std::vector<T> >
+    class mathVec
+    {
+        Cont cont;
+
+    public:
+        // mathVec with initial size
+        constexpr mathVec(const std::size_t n) : cont(n){}
+
+        // mathVec with initial size and value
+        mathVec(const std::size_t n, const double initialValue) : cont(n, initialValue){}
+
+        // Constructor for underlying container
+        constexpr mathVec(const Cont& other) : cont(other){}
+
+        // assignment operator for mathVec of different type
+        template<typename T2, typename R2>
+       constexpr mathVec & operator=(const mathVec<T2, R2>& other){
+            assert(size() == other.size());
+            for (std::size_t i = 0; i < cont.size(); ++i) cont[i] = other[i];
+            return *this;
+        }
+
+        // size of underlying container
+        constexpr std::size_t size() const{
+            return std::size(cont);
+        }
+
+        // index operators
+        constexpr T operator[](const std::size_t i) const{
+            return cont[i];
+        }
+
+        constexpr T& operator[](const std::size_t i){
+            return cont[i];
+        }
+
+        // returns the underlying data
+        const Cont& data() const{
+            return cont;
+        }
+
+        Cont& data(){
+            return cont;
+        }
+    };
+
+    template<typename T, typename Op1, typename Op2>
+    class mathVecAdd
+    {
+        const Op1& op1;
+        const Op2& op2;
+
+    public:
+        constexpr mathVecAdd(const Op1& a, const Op2& b) :op1{a}, op2{b} { }
+
+        constexpr T operator[](const std::size_t i) noexcept
+        {
+            return op1[i] + op2[i];
+        }
+
+        constexpr std::size_t size() const
+        {
+            return std::size(op1);
+        }
+
+    };
+
+    template<typename T, typename Op1, typename Op2>
+    class mathVecMult
+    {
+        const Op1& op1;
+        const Op2& op2;
+
+    public:
+        constexpr mathVecMult(const Op1& a, const Op2& b) :op1{a}, op2{b} { }
+
+        constexpr T operator[](const std::size_t i) noexcept
+        {
+            return op1[i] * op2[i];
+        }
+
+        constexpr std::size_t size() const
+        {
+            return std::size(op1);
+        }
+    };
+
+
+    template<typename T, typename Rng1, typename Rng2>
+    constexpr mathVec<T, mathVecAdd<T, Rng1, Rng2>>
+    operator+(const mathVec<T, Rng1>& a, const mathVec<T, Rng2>& b) noexcept
+    {
+        return mathVec<T, mathVecAdd<T,Rng1,Rng2>>(mathVecAdd<T,Rng1, Rng2>(a.data(), b.data()));
+    }
+
+    template<typename T, typename Rng1, typename Rng2>
+    constexpr mathVec<T, mathVecMult<T, Rng1, Rng2>>
+    operator*(const mathVec<T, Rng1>& a, const mathVec<T, Rng2>& b) noexcept
+    {
+        return mathVec<T, mathVecMult<T,Rng1,Rng2>>(mathVecMult<T,Rng1, Rng2>(a.data(), b.data()));
+    }
+
+    template<typename T>
+    std::ostream& operator<<(std::ostream& os, const mathVec<T>& cont)
+    {
+        for(const auto elem : cont)
+        { os << elem<<' ';}
+        return os;
+    }
 
 
 
